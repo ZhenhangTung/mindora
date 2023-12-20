bb3e2c2a6582cbcc366cd17ab301d5d7a4e41df2
zhenhangtung <zhenhangtung@gmail.com>
2023-12-20 09:03:08 +0800

    fix(ChatUI): turbo and scrolling to the bottom

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -1,18 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["message"]
+  static targets = ["messagesContainer"]
 
   connect() {
-    this.scrollToBottom()
     this.element.addEventListener('turbo:submit-end', () => this.clearMessage()) // clear message after submit
-
     // FIXME: this doesn't work
-    // this.element.addEventListener('turbo:after-stream-render', () => this.scrollToBottom()) // scroll to bottom after render
+    this.element.addEventListener('turbo:after-stream-render', () => this.scrollToBottom()) // scroll to bottom after render
+    this.scrollToBottom()
   }
 
   scrollToBottom() {
-    const element = this.element
+    const element = this.messagesContainerTarget
     element.scrollTop = element.scrollHeight
   }
 
