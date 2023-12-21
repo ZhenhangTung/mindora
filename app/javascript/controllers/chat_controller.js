0219416cbfc011bfec487a896686fa4c7da1c4aa
zhenhangtung <zhenhangtung@gmail.com>
2023-12-21 22:39:52 +0800

    fix(turbo): fix turbo stream

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -1,20 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
-import { Turbo } from "@hotwired/turbo-rails"
 
+// FIXME: auto scrolling doesn't work
 export default class extends Controller {
-  static targets = ["messagesContainer", "messageTextArea", "messages"]
+  static targets = ["messagesContainer", "messageTextArea", "messages", "form"]
 
   connect() {
-    // this.element.addEventListener('turbo:submit-end', () => this.clearMessage()) // clear message after submit
+    this.element.addEventListener('turbo:submit-end', () => this.clearMessage()) // clear message after submit
 
     this.scrollToBottom()
-
-    // FIXME: auto scrolling doesn't work
-    // console.log(this.messagesContainerTarget)
-    // this.messagesContainerTarget.addEventListener('turbo:load', () => this.scrollToBottom()) // scroll to bottom after render
-    // this.observer = new MutationObserver(() => this.scrollToBottom());
-    // this.observer.observe(this.element, { childList: true });
-    // document.addEventListener('turbo:frame-load', () => this.scrollToBottom())
   }
 
   scrollToBottom() {
@@ -26,23 +19,14 @@ export default class extends Controller {
   }
 
   submit(event) {
-    event.preventDefault();
-
     const messageText = this.messageTextAreaTarget.value;
     if (messageText.trim() === "") {
       return; // Prevent empty messages
     }
-    console.log(messageText)
-    //
+    
+    // optimistic UI update
     this.addMessage(messageText)
-    this.clearMessage()
     this.scrollToBottom()
-
-    // TODO: Submit the form data to the server
-    Turbo.visit(this.element.getAttribute('action'), {
-      method: this.element.getAttribute('method'),
-      body: new FormData(this.element)
-    });
   }
 
   addMessage(text) {
@@ -50,7 +34,7 @@ export default class extends Controller {
       <div class="chat chat-end">
         <div class="chat-image avatar">
           <div class="w-10 rounded-full">
-            <img alt="Chat Avatar" src="https://files.olavibe.com/image/DALL%C2%B7E%202023-12-20%2007.31.52%20-%20A%20digital%20portrait%20of%20a%20person%20with%20a%20neutral%20expression%2C%20illuminated%20by%20soft%20blue%20lighting%20on%20one%20side%20of%20the%20face%20and%20warm%20pink%20lighting%20on%20the%20othe.png" />
+            <img alt="Chat Avatar" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
           </div>
         </div>
         <div class="chat-bubble">
