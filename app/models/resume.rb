b7eb740a8c5cead4d20445f1e8800169a5f1905d
zhenhangtung <zhenhangtung@gmail.com>
2024-03-23 10:30:06 +0800

    chore(resume): require work experiences and educations

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -24,6 +24,10 @@ class Resume < ApplicationRecord
       self.phone_number = data[:phone_number] if data.key?(:phone_number)
       self.email = data[:email] if data.key?(:email)
 
+      # Check if required data is present
+      raise "Work experiences data is missing" unless data.key?(:work_experiences) && data[:work_experiences].present?
+      raise "Educations data is missing" unless data.key?(:educations) && data[:educations].present?
+
       # Assuming `data` is a hash with :work_experiences and :educations keys
       work_experiences_attributes = data[:work_experiences].each do |we_data|
         parse_date_fields(we_data)
