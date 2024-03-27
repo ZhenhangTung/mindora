47b6d64685f0a7a8e2e59d1e80874992e16de75e
zhenhangtung <zhenhangtung@gmail.com>
2024-03-27 19:06:09 +0800

    chore(chat_session): fake error

--- app/models/chat_session.rb
+++ app/models/chat_session.rb
@@ -5,4 +5,11 @@ class ChatSession < ApplicationRecord
 
   # Temporary field to map with user's session ID from cookies
   validates :anonymous_user_id, presence: true
+
+  validate :fake_error
+
+  private
+  def fake_error
+    errors.add(:base, "Fake error to simulate failure")
+  end
 end
