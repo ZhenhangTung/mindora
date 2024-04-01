32b62830246600dad78ef5cd3cd286c39f86273c
zhenhangtung <zhenhangtung@gmail.com>
2024-04-01 18:51:03 +0800

    chore(session): update session config & add debug log

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_pmwangwang_session', expire_after: 2.weeks
+Rails.application.config.session_store :cookie_store, key: '_pmwangwang_session', expire_after: 2.weeks, secure: Rails.env.production?
