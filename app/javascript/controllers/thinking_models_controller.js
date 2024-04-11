a44a339ccba914520200fc2ee5802d5f0fa90e1e
zhenhangtung <zhenhangtung@gmail.com>
2024-04-11 19:24:04 +0800

    feat(chat): implement chat history

--- app/javascript/controllers/thinking_models_controller.js
+++ app/javascript/controllers/thinking_models_controller.js
@@ -1,17 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
 
+const localStorageKey = "PMWW_ChatHistory"
+
 export default class extends Controller {
-    static targets = ["model", "input"]
+    static targets = ["model", "input", "messages"]
 
     connect() {
-        this.loadMessages()
+        this.restoreChatFromLocalStorage()
     }
 
-    loadMessages() {
+    restoreChatFromLocalStorage() {
+        let chatHistory = JSON.parse(localStorage.getItem(localStorageKey)) || [];
 
+        chatHistory.forEach(chat => {
+            this.appendMessage(chat.message, chat.type);
+        });
     }
 
     submit(event) {
+        event.preventDefault()
         const selectedModels = this.modelTargets.filter(checkbox => checkbox.checked).map(checkbox => checkbox.value)
         const userInput = this.inputTarget.value
 
@@ -21,6 +28,13 @@ export default class extends Controller {
         }
 
         console.log(data)
+
+        this.saveChatToLocalStorage("user", userInput)
+        this.appendMessage(data.user_input, "user")
+        this.clearInput()
+
+        // TODO: also send chat history to the server
+
         fetch("/chat/thinking_models", {
             method: "POST",
             headers: {
@@ -32,18 +46,46 @@ export default class extends Controller {
             .then(response => response.json())
             .then(data => {
                 console.log("Success:", data)
-                // 处理响应数据
+                this.appendMessage(data.message, "assistant")
+                this.saveChatToLocalStorage("assistant", data.message);
             })
             .catch((error) => {
                 console.error("Error:", error)
             })
     }
 
-    sendMessage(message) {
+    appendMessage(message, type) {
+        const div = document.createElement("div")
+        div.classList.add("flex", "items-start")
+        if (type === "user") {
+            div.classList.add("flex-row-reverse")
+            div.innerHTML = `
+        <img class="ml-2 h-8 w-8 rounded-full" src="https://dummyimage.com/128x128/354ea1/ffffff&text=我" />
+        <div class="min-h-[85px] rounded-b-xl rounded-tl-xl bg-slate-50 p-4 dark:bg-slate-800 sm:min-h-0 sm:max-w-md md:max-w-2xl">
+            <p>${message.split("\n\n").join("</p><p>").split("\n").join("<br>")}</p>
+        </div>
+      `
+        } else {  // assistant
+            div.innerHTML = `
+        <img class="mr-2 h-8 w-8 rounded-full" src="https://dummyimage.com/128x128/363536/ffffff&text=J" />
+        <div class="rounded-b-xl rounded-tr-xl bg-slate-50 p-4 dark:bg-slate-800 sm:max-w-md md:max-w-2xl">
+            <p>${message.split("\n\n").join("</p><p>").split("\n").join("<br>")}</p>
+        </div>
+      `
+        }
 
+        this.messagesTarget.appendChild(div)  // 确保你的 HTML 里有 data-thinking-model-target="messages"
     }
 
-    addMessage(content) {
+    saveChatToLocalStorage(type, message) {
+        let chatHistory = JSON.parse(localStorage.getItem(localStorageKey)) || [];
 
+        chatHistory.push({ type, message });
+
+        localStorage.setItem(localStorageKey, JSON.stringify(chatHistory));
+    }
+
+    clearInput() {
+        this.inputTarget.value = ""
     }
 }
