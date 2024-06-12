64fa10e4bf973acdac46aab596715bf944a5b2be
zhenhangtung <zhenhangtung@gmail.com>
2024-06-12 08:47:34 +0800

    feat(questionnaires): init controller and view

---
+++ app/controllers/works/questionnaires_controller.rb
@@ -0,0 +1,27 @@
+class Works::QuestionnairesController < ApplicationController
+  before_action :authenticate_user
+
+  def index
+    @questionnaires = []
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def chat
+
+  end
+end
